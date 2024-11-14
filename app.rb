4a12e3948f57e3d74aa0d651887e357fb2c86d02
churquh2003 <curquhart@uchicago.edu>
2024-11-14 01:21:31 +0000

    commit 2

--- app.rb
+++ app.rb
@@ -1,9 +1,41 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get '/' do
+  erb :index
+end
+
+get '/rock' do
+  @player_move = "rock"
+  @computer_move = ["rock", "paper", "scissors"].sample
+  determine_result
+  erb :result
+end
+
+get '/paper' do
+  @player_move = "paper"
+  @computer_move = ["rock", "paper", "scissors"].sample
+  determine_result
+  erb :result
+end
+
+get '/scissors' do
+  @player_move = "scissors"
+  @computer_move = ["rock", "paper", "scissors"].sample
+  determine_result
+  erb :result
+end
+
+helpers do
+  def determine_result
+    if @player_move == @computer_move
+      @result = "We tied!"
+    elsif (@player_move == "rock" && @computer_move == "scissors") ||
+          (@player_move == "paper" && @computer_move == "rock") ||
+          (@player_move == "scissors" && @computer_move == "paper")
+      @result = "We won!"
+    else
+      @result = "We lost!"
+    end
+  end
 end
